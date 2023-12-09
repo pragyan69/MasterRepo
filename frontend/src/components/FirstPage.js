1196d52bab7c61ce19257e1026f6d950ef2051d5
pragyan69 <prajyanqw@gmail.com>
2023-12-09 13:44:34 +0530

    buttons added in react component

--- frontend/src/components/FirstPage.js
+++ frontend/src/components/FirstPage.js
@@ -1,112 +1,34 @@
 import React from 'react'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const FirstPage = () => {
     return (
-        <div className="shadow-2xl pl-16 pb-8 rounded-[30px] border-[3px] border-solid border-sky-200 max-md:pl-5">
-          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-            <div className="flex flex-col items-stretch w-[46%] max-md:w-full max-md:ml-0">
-              <div className="z-[1] flex flex-col items-stretch my-auto max-md:max-w-full max-md:mt-10">
-                <div className="flex w-full items-stretch justify-between gap-5 pr-20 max-md:max-w-full max-md:flex-wrap max-md:pr-5">
-                  <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/d679a9b2409f5187638bc45896e1014f8a063de90c540c5a9edd7e1af111118c?apiKey=181b35ca247b4ffeb33b054197febc24&"
-                    className="aspect-[1.47] object-contain object-center w-[69px] overflow-hidden shrink-0 max-w-full"
-                  />
-                  <div className="self-center flex items-stretch justify-between gap-5 my-auto">
-                    <div className="text-gray-500 text-xs font-bold tracking-widest uppercase whitespace-nowrap">
-                      home
-                    </div>
-                    <div className="text-gray-700 text-xs font-extrabold tracking-widest uppercase whitespace-nowrap">
-                      products
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col mt-48 pr-5 max-md:max-w-full max-md:mt-10">
-                  <div className="justify-center text-black text-2xl font-medium leading-5 tracking-[18.11px] uppercase ml-6 self-start max-md:max-w-full">
-                    let’s Make Easy
-                  </div>
-                  <div className="justify-center text-black text-9xl font-semibold leading-6 tracking-tighter uppercase self-stretch ml-3.5 mt-10 max-md:max-w-full max-md:text-4xl max-md:leading-loose">
-                    Connection
-                  </div>
-                  <div className="text-zinc-700 text-lg font-medium leading-8 tracking-normal w-[245px] max-w-full ml-3.5 mt-16 self-start max-md:ml-2.5 max-md:mt-10">
-                    Simple and sleek design with users in mind.
-                  </div>
-                  <div className="flex w-[378px] max-w-full items-stretch justify-between gap-5 mt-14 self-start max-md:mt-10">
-                    <div className="text-white text-sm font-bold tracking-[2.01px] uppercase whitespace-nowrap grow justify-center items-stretch pl-10 pr-12 py-5 rounded-lg border-[1.737px] border-solid border-slate-400 max-md:px-5">
-                      Create
-                    </div>
-                    <div className="text-white text-sm font-bold tracking-[2.01px] uppercase whitespace-nowrap grow justify-center pl-14 pr-16 py-5 rounded-lg border-[1.737px] border-solid border-slate-400 items-start max-md:px-5">
-                      Join
-                    </div>
-                  </div>
-                  <div className="flex items-stretch gap-3 mt-64 self-start max-md:mt-10">
-                    <img
-                      loading="lazy"
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/c65bfa15fa77c8b8e1c5db6bc6734aeff0235f5cec90692c580d68e596313258?apiKey=181b35ca247b4ffeb33b054197febc24&"
-                      className="aspect-[0.48] object-contain object-center w-[13px] stroke-[2.325px] stroke-zinc-800 overflow-hidden shrink-0 max-w-full"
-                    />
-                    <div className="text-black text-opacity-60 text-lg font-semibold leading-4 capitalize self-center grow whitespace-nowrap my-auto">
-                      Prev
-                    </div>
-                  </div>
-                </div>
+        <div className="shadow-2xl pl-16 pb-8 rounded-[30px] max-md:pl-5">
+          <div className="flex flex-col items-stretch min-h-screen justify-center">
+            <div className="text-black text-2xl font-medium leading-5 tracking-[18.11px] uppercase ml-6">
+              let’s Make Easy
+            </div>
+            <div className="text-yellow text-9xl font-semibold leading-6 tracking-tighter uppercase ml-3.5 mt-10 max-md:text-4xl max-md:leading-loose">
+              Connection
+            </div>
+            <div className="text-zinc-700 text-lg font-medium leading-8 tracking-normal ml-3.5 mt-16 max-md:ml-2.5 max-md:mt-10">
+              Simple and sleek design with users in mind.
+            </div>
+            <div className="ml-6 mt-14 max-md:mt-10">
+              <div className="flex gap-4 px-5">
+              <Stack spacing={2} direction="row">
+      
+             <Button variant="contained">Create Room</Button>
+             <Button variant="contained">Join Room</Button>
+      
+            </Stack>
               </div>
             </div>
-            <div className="flex flex-col items-stretch w-[54%] ml-5 max-md:w-full max-md:ml-0">
-              <div className="w-full pl-11 rounded-[50%] max-md:max-w-full max-md:pl-5">
-                <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                  <div className="flex flex-col items-stretch w-[24%] max-md:w-full max-md:ml-0">
-                    <div className="flex flex-col mt-16 items-end max-md:mt-10">
-                      <div className="text-gray-500 text-xs font-bold tracking-widest uppercase whitespace-nowrap">
-                        gallery
-                      </div>
-                      <div className="self-stretch flex items-stretch justify-between gap-3 mt-[830px] pr-7 max-md:justify-center max-md:mt-10 max-md:pr-5">
-                        <div className="flex w-2.5 shrink-0 h-2.5 flex-col rounded-[50%]" />
-                        <div className="flex w-2.5 shrink-0 h-2.5 flex-col rounded-[50%]" />
-                        <div className="flex w-2.5 shrink-0 h-2.5 flex-col rounded-[50%]" />
-                        <div className="flex w-2.5 shrink-0 h-2.5 flex-col rounded-[50%]" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-stretch w-[42%] ml-5 max-md:w-full max-md:ml-0">
-                    <div className="flex items-center justify-between gap-5 mt-14 max-md:mt-10">
-                      <div className="text-gray-500 text-xs font-bold tracking-widest uppercase grow whitespace-nowrap my-auto">
-                        contact
-                      </div>
-                      <div className="text-white text-xs font-bold tracking-widest uppercase whitespace-nowrap self-stretch grow justify-center items-stretch px-8 py-4 rounded-lg border-[1.5px] border-solid border-emerald-300 max-md:px-5">
-                        login
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-stretch w-[17%] ml-5 max-md:w-full max-md:ml-0">
-                    <div className="flex items-center gap-2.5 mt-[902px] max-md:mt-10">
-                      <div className="text-black text-opacity-60 text-lg font-semibold leading-4 capitalize grow whitespace-nowrap my-auto">
-                        Next
-                      </div>
-                      <img
-                        loading="lazy"
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/aee62fb337fd0fb5c48ceba52663c05bd7ec8fa42080124f80beffb0641f8387?apiKey=181b35ca247b4ffeb33b054197febc24&"
-                        className="aspect-[0.48] object-contain object-center w-[13px] stroke-[2.325px] stroke-zinc-800 overflow-hidden self-stretch shrink-0 max-w-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-stretch w-[17%] ml-5 max-md:w-full max-md:ml-0">
-                    <div className="flex flex-col items-stretch max-md:mt-10">
-                      <div className="flex shrink-0 h-[491px] flex-col" />
-                      <div className="flex flex-col pr-16 pt-12 pb-8 items-start max-md:pr-5">
-                        <div className="flex shrink-0 h-[70px] flex-col mt-80 rounded-[50%] max-md:mt-10" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {/* Additional content can be added here */}
           </div>
         </div>
-      );
+    );
 }
 
-export default FirstPage
-
-
-
+export default FirstPage;
