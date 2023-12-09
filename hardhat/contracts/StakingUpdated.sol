ac85335fd6391f87094319caa47566f432358b91
pragyan69 <prajyanqw@gmail.com>
2023-12-09 14:28:10 +0530

    smart contract updated

--- hardhat/contracts/StakingUpdated.sol
+++ hardhat/contracts/StakingUpdated.sol
@@ -71,4 +71,103 @@ contract StakingUpdated {
             memberList.push(member); // Add to member list
         }
     }
+
+    // function to revoke members
+    function revokeMember(address member) public onlyOwner {
+        if (isMember[member]) {
+            isMember[member] = false;
+
+            // Remove from member list
+            for (uint i = 0; i < memberList.length; i++) {
+                if (memberList[i] == member) {
+                    memberList[i] = memberList[memberList.length - 1];
+                    memberList.pop();
+                    break;
+                }
+            }
+        }
+    }
+
+
+// function to submit ideas
+    function submitIdea(uint256 roomId, string memory idea) public {
+        require(isMember[msg.sender], "Only members can submit ideas");
+        require(roomId < roomCount, "Invalid room ID");
+        bool isMemberOfRoom = false;
+        for (uint i = 0; i < rooms[roomId].members.length; i++) {
+            if (rooms[roomId].members[i] == msg.sender) {
+                isMemberOfRoom = true;
+                break;
+            }
+        }
+        require(isMemberOfRoom, "Member not part of the room");
+        roomIdeas[roomId].push(idea);
+    }
+
+    // Function to get ideas for a specific room
+    function getRoomIdeas(uint256 roomId) public view returns (string[] memory) {
+        require(roomId < roomCount, "Invalid room ID");
+        return roomIdeas[roomId];
+    }
+
+
+// function to create room
+  function createRoom(string memory name, string memory description, uint256 memberLimit, uint256 validityInDays) public {
+        require(isMember[msg.sender], "Only members can create rooms");
+        require(memberLimit > 0, "Member limit must be positive");
+        require(validityInDays > 0, "Validity must be positive");
+
+        Room storage room = rooms[roomCount++];
+        room.name = name;
+        room.description = description;
+        room.memberLimit = memberLimit;
+        room.validity = block.timestamp + (validityInDays * 1 days);
+        room.members.push(msg.sender);
+    }
+
+// this function , will return the room name , correspond to the owner of the room
+    function getRooms() public view returns (Room[] memory) {
+        Room[] memory allRooms = new Room[](roomCount);
+        for (uint i = 0; i < roomCount; i++) {
+            allRooms[i] = rooms[i];
+        }
+        return allRooms;
+    }
+
+// function to join room
+    function joinRoom(uint256 roomId) public {
+        require(isMember[msg.sender], "Only members can join rooms");
+        require(roomId < roomCount, "Invalid room ID");
+
+        Room storage room = rooms[roomId];
+
+        // Check if the room has not exceeded its member limit
+        require(room.members.length < room.memberLimit, "Room is full");
+
+        // Add member to the room
+        room.members.push(msg.sender);
+    }
+
+
+// function to return all the members with their address
+    function getAllMembers() public view returns (address[] memory) {
+        return memberList;
+    }
+
+// this function will return all the rooms with the ID
+    function listAllRooms() public view returns (Room[] memory) {
+        Room[] memory allRooms = new Room[](roomCount);
+        for (uint i = 0; i < roomCount; i++) {
+            allRooms[i] = rooms[i];
+        }
+        return allRooms;
+    }
+
+     // Add a function to get members of a specific room
+    function getRoomMembers(uint256 roomId) public view returns (address[] memory) {
+        require(roomId < roomCount, "Invalid room ID");
+        return rooms[roomId].members;
+    }
+
+    
 }
