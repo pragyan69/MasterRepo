12cb2a02a1452db0bad24ba0e8a0b28a61e4f5b5
pragyan69 <prajyanqw@gmail.com>
2023-12-09 13:54:45 +0530

    frontend updated

--- frontend/src/components/FirstPage.js
+++ frontend/src/components/FirstPage.js
@@ -4,25 +4,24 @@ import Button from '@mui/material/Button';
 
 const FirstPage = () => {
     return (
-        <div className="shadow-2xl pl-16 pb-8 rounded-[30px] max-md:pl-5">
-          <div className="flex flex-col items-stretch min-h-screen justify-center">
-            <div className="text-black text-2xl font-medium leading-5 tracking-[18.11px] uppercase ml-6">
+        // Adjusted gradient background classes for a whitish-blue gradient
+        <div className="bg-gradient-to-br from-blue-50 via-blue-100 to-blue-50 shadow-2xl pl-16 pb-8  max-md:pl-5 ">
+          <div className="flex flex-col items-start min-h-screen justify-start pt-10">
+            <div className="text-black text-2xl font-medium leading-5 tracking-[18.11px] uppercase ml-6 mt-6">
               let’s Make Easy
             </div>
-            <div className="text-yellow text-9xl font-semibold leading-6 tracking-tighter uppercase ml-3.5 mt-10 max-md:text-4xl max-md:leading-loose">
+            <div className="text-yellow text-9xl font-semibold leading-none tracking-tighter uppercase ml-3.5 mt-4 max-md:text-4xl max-md:leading-loose">
               Connection
             </div>
-            <div className="text-zinc-700 text-lg font-medium leading-8 tracking-normal ml-3.5 mt-16 max-md:ml-2.5 max-md:mt-10">
+            <div className="text-gray-700 text-lg font-medium leading-8 tracking-normal ml-3.5 mt-6 max-md:ml-2.5 max-md:mt-4">
               Simple and sleek design with users in mind.
             </div>
-            <div className="ml-6 mt-14 max-md:mt-10">
-              <div className="flex gap-4 px-5">
-              <Stack spacing={2} direction="row">
-      
-             <Button variant="contained">Create Room</Button>
-             <Button variant="contained">Join Room</Button>
-      
-            </Stack>
+            <div className="ml-6 mt-8 max-md:mt-6">
+              <div className="flex gap-4">
+                <Stack spacing={2} direction="row">
+                  <Button variant="contained">Create Room</Button>
+                  <Button variant="contained">Join Room</Button>
+                </Stack>
               </div>
             </div>
             {/* Additional content can be added here */}
